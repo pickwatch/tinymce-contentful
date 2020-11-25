e4403df24cce181685677e45823098ab8e8d5eb3
Ryan Jones <ryan@pickwatch.com>
2020-11-25 10:32:46 +0000

    Enable browser spell check

--- src/tinymce-for-contentful.js
+++ src/tinymce-for-contentful.js
@@ -27,6 +27,7 @@ window.contentfulExtension.init(function(api) {
       autoresize_bottom_margin: 15,
       resize: false,
       image_caption: true,
+      browser_spellcheck: true,
       init_instance_callback : function(editor) {
         var listening = true;
 
